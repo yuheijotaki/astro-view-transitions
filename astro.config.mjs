3c698aae6de09de8116bb57b988f46c0ae272dae
Yuhei Jotaki <yuheijotaki@gmail.com>
2024-05-09 16:45:03 +0900

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from 'astro/config';
 
+// https://astro.build/config
 export default defineConfig({
   site: 'https://yuheijotaki.github.io',
   base: '/astro-view-transitions',
