057dc4100bc56fb62801629120b8c5ec3191b514
Yuhei Jotaki <yuheijotaki@gmail.com>
2024-05-09 16:34:48 +0900

    add GitHub Pages settings

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yuheijotaki.github.io',
+  base: '/astro-view-transitions',
   server: {
     host: true,
     open: true,
